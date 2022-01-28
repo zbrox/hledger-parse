c4092e0b01827214612938499d4d12f3127be149
Rostislav Raykov <me@zbrox.com>
2022-01-28 22:41:50 +0100

    fix comment parser
    one of the comment characters was wrong

--- src/parsers/comments.rs
+++ src/parsers/comments.rs
@@ -7,7 +7,7 @@ use super::utils::is_char_newline;
 
 pub fn parse_comment(input: &str) -> IResult<&str, &str> {
     preceded(
-        alt((char('#'), char(';'), char('|'))),
+        alt((char('#'), char(';'), char('*'))),
         preceded(space0, take_till(is_char_newline)),
     )(input)
 }
@@ -20,8 +20,8 @@ mod tests {
     fn test_parse_comment() {
         assert_eq!(parse_comment("; comment\n"), Ok(("\n", "comment")));
         assert_eq!(parse_comment("; comment"), Ok(("", "comment")));
-        assert_eq!(parse_comment("| comment\n"), Ok(("\n", "comment")));
-        assert_eq!(parse_comment("| comment"), Ok(("", "comment")));
+        assert_eq!(parse_comment("* comment\n"), Ok(("\n", "comment")));
+        assert_eq!(parse_comment("* comment"), Ok(("", "comment")));
         assert_eq!(parse_comment("# comment\n"), Ok(("\n", "comment")));
         assert_eq!(parse_comment("# comment"), Ok(("", "comment")));
     }
