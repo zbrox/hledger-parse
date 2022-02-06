872f0de61c346f718b0d3e77ff7a075afa1b52aa
Rostislav Raykov <me@zbrox.com>
2022-02-06 16:25:38 +0100

    add structures related to amount
    including a test for the amount sign multiplier

--- src/types.rs
+++ src/types.rs
@@ -12,3 +12,34 @@ pub struct Description {
     pub payee: Option<String>,
     pub note: Option<String>,
 }
+
+pub enum AmountSign {
+    Plus,
+    Minus,
+}
+
+impl AmountSign {
+    pub fn multiplier(&self) -> i8 {
+        match *self {
+            AmountSign::Plus => 1,
+            AmountSign::Minus => -1,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Amount {
+    pub currency: String,
+    pub value: i32,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::AmountSign;
+
+    #[test]
+    fn test_amount_sign_multiplier() {
+        assert_eq!(AmountSign::Plus.multiplier(), 1);
+        assert_eq!(AmountSign::Minus.multiplier(), -1);
+    }
+}
