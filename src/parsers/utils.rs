4485298349ff000c2d30f651c21eaf38f2310912
Rostislav Raykov <me@zbrox.com>
2022-02-06 23:30:55 +0100

    add is_char_space utility function

--- src/parsers/utils.rs
+++ src/parsers/utils.rs
@@ -1,6 +1,6 @@
 use nom::{
     bytes::complete::take_until,
-    character::{complete::char, complete::space0, is_digit, is_newline},
+    character::{complete::char, complete::space0, is_digit, is_newline, is_space},
     sequence::{delimited, preceded, terminated},
     IResult,
 };
@@ -11,6 +11,10 @@ pub fn is_char_newline(char: char) -> bool {
 
 pub fn is_char_digit(char: char) -> bool {
     is_digit(char as u8)
+}
+
+pub fn is_char_space(char: char) -> bool {
+    is_space(char as u8)
 }
 
 pub fn in_quotes(input: &str) -> IResult<&str, &str> {
