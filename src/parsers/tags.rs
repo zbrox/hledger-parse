750aec75063903efc2cec7e9b26dd7db1aac3075
Rostislav Raykov <me@zbrox.com>
2022-02-10 22:12:23 +0100

    fix tag parser
    missed the part about what is a valid tag and value characters

--- src/parsers/tags.rs
+++ src/parsers/tags.rs
@@ -1,7 +1,6 @@
 use nom::{
     bytes::complete::{tag, take_while},
-    character::complete::alphanumeric1,
-    combinator::opt,
+    combinator::rest,
     sequence::terminated,
     IResult,
 };
@@ -11,14 +10,22 @@ use crate::types::Tag;
 use super::utils::is_char_alphanumeric;
 
 pub fn parse_tag(input: &str) -> IResult<&str, Tag> {
-    let (tail, name) = terminated(take_while(|c| is_char_alphanumeric(c) || c == '-'), tag(":"))(input)?;
-    let (tail, value) = opt(alphanumeric1)(tail)?;
+    let (tail, name) = terminated(
+        take_while(|c| is_char_alphanumeric(c) || c == '-'),
+        tag(":"),
+    )(input)?;
+    let (tail, value) = rest(tail)?;
+    let value = value.trim();
+    let value = match value.len() {
+        0 => None,
+        _ => Some(value),
+    };
 
     Ok((
         tail,
         Tag {
             name: name.into(),
-            value: value.map(str::to_string),
+            value: value.map(str::trim).map(str::to_string),
         },
     ))
 }
@@ -29,7 +36,13 @@ mod tests {
 
     #[test]
     fn test_parse_tag_with_space() {
-        assert_eq!(parse_tag("not a tag:"), Err(nom::Err::Error(nom::error::Error { input: " a tag:", code: nom::error::ErrorKind::Tag })))
+        assert_eq!(
+            parse_tag("not a tag:"),
+            Err(nom::Err::Error(nom::error::Error {
+                input: " a tag:",
+                code: nom::error::ErrorKind::Tag
+            }))
+        )
     }
 
     #[test]
