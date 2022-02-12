d709ca9f57ffd9c3b3eee3ea73e9b4dc6fc7066e
Rostislav Raykov <me@zbrox.com>
2022-02-12 19:49:59 +0100

    add helper function to parse multiple line comments

--- src/parsers/comments.rs
+++ src/parsers/comments.rs
@@ -1,13 +1,22 @@
 use nom::{
-    branch::alt, character::complete::char, character::complete::space0, combinator::rest,
-    sequence::preceded, IResult,
+    branch::alt,
+    character::complete::char,
+    character::complete::{line_ending, not_line_ending, space0},
+    combinator::{opt, rest},
+    multi::many0,
+    sequence::{preceded, terminated},
+    IResult,
 };
 
 pub fn parse_line_comment(input: &str) -> IResult<&str, &str> {
     preceded(
         alt((char('#'), char(';'), char('*'))),
-        preceded(space0, rest),
+        preceded(space0, not_line_ending),
     )(input)
+}
+
+pub fn parse_line_comments(input: &str) -> IResult<&str, Vec<&str>> {
+    many0(terminated(parse_line_comment, opt(line_ending)))(input)
 }
 
 pub fn parse_transaction_comment(input: &str) -> IResult<&str, &str> {
@@ -17,6 +26,8 @@ pub fn parse_transaction_comment(input: &str) -> IResult<&str, &str> {
 #[cfg(test)]
 mod tests {
     use crate::parsers::comments::{parse_line_comment, parse_transaction_comment};
+
+    use super::parse_line_comments;
 
     #[test]
     fn test_parse_line_comment() {
@@ -34,6 +45,18 @@ mod tests {
                 input: "# comment",
                 code: nom::error::ErrorKind::Char
             }))
+        );
+    }
+
+    #[test]
+    fn test_parse_multiple_line_comments() {
+        let input = r#"; comment1
+;comment2
+;
+;  comment3"#;
+        assert_eq!(
+            parse_line_comments(input),
+            Ok(("", vec!["comment1", "comment2", "", "comment3"]))
         );
     }
 }
