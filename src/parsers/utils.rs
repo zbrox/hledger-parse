31898a50d64fe11ba67320cb3af88700bc94ff9a
Rostislav Raykov <me@zbrox.com>
2022-05-29 17:29:39 +0200

    add is_char_newline helper

--- src/parsers/utils.rs
+++ src/parsers/utils.rs
@@ -1,9 +1,13 @@
 use nom::{
     bytes::complete::take_until,
-    character::{complete::char, complete::space0, is_digit, is_space},
+    character::{complete::char, complete::space0, is_digit, is_space, is_newline},
     sequence::{delimited, preceded, terminated},
     IResult,
 };
+
+pub fn is_char_newline(char: char) -> bool {
+    is_newline(char as u8)
+}
 
 pub fn is_char_space(char: char) -> bool {
     is_space(char as u8)
