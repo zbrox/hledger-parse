a2edade8370012e1b3b496f4c858e8d9a34da151
Rostislav Raykov <me@zbrox.com>
2022-06-23 17:17:00 +0200

    implement display trait for tag type

--- src/tag/types.rs
+++ src/tag/types.rs
@@ -1,5 +1,16 @@
+use std::fmt::Display;
+
 #[derive(PartialEq, Debug, Clone)]
 pub struct Tag {
     pub name: String,
     pub value: Option<String>,
+}
+
+impl Display for Tag {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.value.as_ref() {
+            Some(value) => write!(f, "{}:{}", self.name, value),
+            None => write!(f, "{}:", self.name),
+        }
+    }
 }
