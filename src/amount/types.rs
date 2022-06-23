b25656b5ff99c593f145d5dcd02ee0af0e3660ed
Rostislav Raykov <me@zbrox.com>
2022-06-23 16:33:53 +0200

    add some doc tests for amount

--- src/amount/types.rs
+++ src/amount/types.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use rust_decimal::Decimal;
 
 #[derive(PartialEq)]
@@ -6,8 +8,27 @@ pub enum AmountSign {
     Minus,
 }
 
+/// Stores information for an amount
+///
+/// Defaults to displaying amounts in the format `<VALUE> <CURRENCY>`, using a dot as the decimal separator.
+///
+/// # Example:
+///
+/// ```
+/// use rust_decimal_macros::dec;
+/// use hledger_parse::Amount;
+///
+/// let amount = Amount { currency: "EUR".to_string(), value: dec!(19.99) };
+/// assert_eq!("19.99 EUR", format!("{}", amount));
+/// ```
 #[derive(PartialEq, Debug, Clone)]
 pub struct Amount {
     pub currency: String,
     pub value: Decimal,
+}
+
+impl Display for Amount {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.value, self.currency)
+    }
 }
