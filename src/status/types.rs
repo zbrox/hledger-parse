9cd632b63eb8033a77bdb92487a018ec817b3c9a
Rostislav Raykov <me@zbrox.com>
2022-06-23 16:46:41 +0200

    fix display trait of status type

--- src/status/types.rs
+++ src/status/types.rs
@@ -11,8 +11,8 @@ impl Display for Status {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
             Status::Unmarked => write!(f, ""),
-            Status::Pending => write!(f, " ! "),
-            Status::Cleared => write!(f, " * "),
+            Status::Pending => write!(f, "!"),
+            Status::Cleared => write!(f, "*"),
         }
     }
 }
