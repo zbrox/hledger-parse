360ce2fbee2258bf09a17d80196e4db1c2125942
Rostislav Raykov <me@zbrox.com>
2022-06-23 16:55:42 +0200

    implement display trait for posting type

--- src/posting/types.rs
+++ src/posting/types.rs
@@ -9,6 +9,18 @@ pub struct Posting {
     pub total_price: Option<Amount>,
 }
 
+impl Display for Posting {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match (self.amount.as_ref(), self.unit_price.as_ref(), self.total_price.as_ref()) {
+            (None, None, None) => write!(f, "  {} {}", self.status, self.account_name),
+            (Some(amount), None, None) => write!(f, "  {} {}  {}", self.status, self.account_name, amount),
+            (Some(amount), Some(unit_price), None) => write!(f, "  {} {}  {} @ {}", self.status, self.account_name, amount, unit_price),
+            (Some(amount), None, Some(total_price)) => write!(f, "  {} {}  {} @@ {}", self.status, self.account_name, amount, total_price),
+            _ => unreachable!()
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct PostingComplexAmount {
     pub amount: Option<Amount>,
