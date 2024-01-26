c4ef45affdce70ae39517b08ece80e9fc2d32ee4
Rostislav Raykov <me@zbrox.com>
2024-01-26 18:03:36 +0100

    fix: tag parser
    should now cover all the variants of tags, previously wasn’t parsing tags in quotes and with special characters

--- src/tag/parsers.rs
+++ src/tag/parsers.rs
@@ -1,17 +1,21 @@
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_while},
     sequence::terminated,
+    IResult,
 };
 
-use crate::{utils::is_char_alphanumeric, HLParserIResult};
+use crate::{utils::in_quotes, HLParserIResult};
 
 use super::types::Tag;
 
+fn parse_tag_name_no_space(input: &str) -> IResult<&str, &str> {
+    take_while(|c| c != ' ' && c != '\t' && c != ':')(input)
+}
+
 pub fn parse_tag(input: &str) -> HLParserIResult<&str, Tag> {
-    let (tail, name) = terminated(
-        take_while(|c| is_char_alphanumeric(c) || c == '-'),
-        tag(":"),
-    )(input)?;
+    let (tail, name) = terminated(alt((in_quotes, parse_tag_name_no_space)), tag(":"))(input)
+        .map_err(nom::Err::convert)?;
     let (tail, value) = take_while(|c| c != ',')(tail)?;
     let value = value.trim();
     let value = match value.len() {
