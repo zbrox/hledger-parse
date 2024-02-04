d9c322b3318dc51b58ee970a1343826da7022943
Rostislav Raykov <me@zbrox.com>
2024-02-04 20:20:54 +0100

    some tweaks

--- src/comment/parsers.rs
+++ src/comment/parsers.rs
@@ -1,15 +1,11 @@
 use winnow::{
     ascii::{space0, till_line_ending},
-    combinator::{alt, eof, preceded},
+    combinator::{alt, preceded},
     PResult, Parser,
 };
 
 pub fn parse_line_comment<'s>(input: &mut &'s str) -> PResult<&'s str> {
-    preceded(
-        alt(('#', ';', '*')),
-        preceded(space0, alt((till_line_ending, eof))),
-    )
-    .parse_next(input)
+    preceded(alt(('#', ';', '*')), preceded(space0, till_line_ending)).parse_next(input)
 }
 
 pub fn parse_transaction_comment<'s>(input: &mut &'s str) -> PResult<&'s str> {
